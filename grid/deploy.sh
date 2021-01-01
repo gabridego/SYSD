283cbf1bb38e51f707b807cec6a2e7055ad2dddb
Gabriele Degola <gabriele.degola@grenoble-inp.org>
2021-01-01 17:59:02 +0100

    update deployment

--- grid/deploy.sh
+++ grid/deploy.sh
@@ -2,9 +2,9 @@
 
 PWD="`(cd $(dirname \"$0\") && pwd )`"
 TAKTUK="${PWD}/taktuk.sh"
-PLAYBOOK="${PWD}/"
+# PLAYBOOK="${PWD}/provision.yml"
 
 kadeploy3 -f $OAR_NODE_FILE -e debian10-x64-nfs -k
 
 # connect to reserved nodes as root and with self-propagation
-taktuk -l root -s -o connector -o status -o output='"$host: $line\n"' -f <( uniq $OAR_FILE_NODES ) broadcast exec [ $TAKTUK $PLAYBOOK ]
+taktuk -l root -s -o connector -o status -o output='"$host: $line\n"' -f <( uniq $OAR_FILE_NODES ) broadcast exec [ $TAKTUK ]
--- grid/taktuk.sh
+++ grid/taktuk.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sl "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
 apt update
-apt --yes install ansible
-ansible-playbook provision.yml
+# apt --yes install ansible
+apt --yes install sbt scala
+# do we really need ansible?
+# ansible-playbook $1
