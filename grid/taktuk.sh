45456cbba9f2e44e777cb9d15a845ce1515479f4
Gabriele Degola <gabriele.degola@grenoble-inp.org>
2021-01-03 11:51:59 +0100

    add sample makefile and install build tools

--- grid/taktuk.sh
+++ grid/taktuk.sh
@@ -4,6 +4,7 @@ echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.li
 curl -sl "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
 apt update
 # apt --yes install ansible
-apt --yes install sbt scala
+apt --yes install build-essential sbt scala blender
 # do we really need ansible?
 # ansible-playbook $1
+
