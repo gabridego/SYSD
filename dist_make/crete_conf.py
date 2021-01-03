ef13265ea5bc6870beddba67416be0b918216575
Gabriele Degola <gabriele.degola@grenoble-inp.org>
2021-01-03 18:43:29 +0100

    modified script to access ips in nodes

--- dist_make/crete_conf.py
+++ dist_make/crete_conf.py
@@ -1,18 +1,26 @@
 import os, subprocess
 
+# get nodes' hostnames
 nodes = set()
 with open(os.environ['OAR_NODE_FILE'], "r") as f:
     for line in f:
         #nodes.add('"akka://ClusterSystem@' + line.strip() + '"')
         nodes.add(line.strip())
 
+
+# get nodes's ips and save them in a file to access from nodes (nfs)
 node_ips = []
-for node in nodes:
-    command = "dig +short " + node
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    node_ips.append('"akka://ClusterSystem@' + process.communicate()[0].strip() + '"')
+with open("nodes.txt", "w") as out:
+    for node in nodes:
+        command = "dig +short " + node
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        ip = process.communicate()[0]
+        node_ips.append('"akka://ClusterSystem@' + ip.strip() + '"')
+
+        out.write(ip)
 
 
+# convert to string in required format
 nodes_list = '[' + ', '.join(node_ips) + ']'
 # print(nodes_list)
 
