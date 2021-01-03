cc31f0a4c393b91b6ae58ac6bfe587acef70dc94
Gabriele Degola <gabriele.degola@grenoble-inp.org>
2021-01-03 17:33:04 +0100

    replace names with ips in configuration script

--- dist_make/crete_conf.py
+++ dist_make/crete_conf.py
@@ -1,11 +1,19 @@
-import os
+import os, subprocess
 
 nodes = set()
 with open(os.environ['OAR_NODE_FILE'], "r") as f:
     for line in f:
-        nodes.add('"akka://ClusterSystem@' + line.strip() + '"')
+        #nodes.add('"akka://ClusterSystem@' + line.strip() + '"')
+        nodes.add(line.strip())
 
-nodes_list = '[' + ', '.join(nodes) + ']'
+node_ips = []
+for node in nodes:
+    command = "dig +short " + node
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    node_ips.append('"akka://ClusterSystem@' + process.communicate()[0].strip() + '"')
+
+
+nodes_list = '[' + ', '.join(node_ips) + ']'
 # print(nodes_list)
 
 if not os.path.exists("./src/main/resources"):
