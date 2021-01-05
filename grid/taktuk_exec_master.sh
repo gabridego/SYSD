990325acc68f18bc901160f107a33eb85b037ec0
Sergio Giardina <sergio.giardina@grenoble-inp.org>
2021-01-05 12:53:05 +0100

    Update taktuk_exec_master.sh

--- grid/taktuk_exec_master.sh
+++ grid/taktuk_exec_master.sh
@@ -1,3 +1,4 @@
 cd ./systeme-distribues/akka-sample-cluster-scala
-command="runMain sample.cluster.simple.App "$1" 25251"
-sbt $command 
+#command="runMain sample.cluster.simple.App "$1" 25251"
+#sbt $command 
+sbt "runMain sample.cluster.simple.App 25251"
